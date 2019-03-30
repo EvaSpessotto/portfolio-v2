889df791610cb008bae6767fc3c4b5131cbedaa4
Spessotto <eva@spessotto.com>
2019-03-30 18:52:47 +0100

    Add contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,45 @@
 import React, { Component } from "react"
+import { Container, Row, Col } from "reactstrap"
+import "./contact.scss"
+import socialLinks from "../socialLinks"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 import Layout from "../components/Layout"
+import ContactForm from "../components/Contact/ContactForm"
 
 class contact extends Component {
   render() {
-    return <Layout>Hello, contact page</Layout>
+    return (
+      <Layout>
+        <Container id="contact-links">
+          <Row>
+            <Col lg="6" className="offset-lg-3">
+              <h2>Retrouvez-moi sur les réseaux...</h2>
+              <div className="line" />
+            </Col>
+          </Row>
+          <Row className="mt-4">
+            <Col className="offset-lg-3">
+              {socialLinks.map((socialLink, key) => {
+                const { link, icon } = socialLink
+                return (
+                  <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    key={key}
+                  >
+                    <i className={`${icon} ${socialLink.class} fa-3x mr-3`} />
+                  </a>
+                )
+              })}
+            </Col>
+          </Row>
+        </Container>
+
+        <ContactForm />
+      </Layout>
+    )
   }
 }
 
---
+++ src/socialLinks.js
@@ -0,0 +1,22 @@
+const socialLinks = [
+  {
+    name: "Github",
+    class: "github",
+    icon: "fab fa-github-square",
+    link: "https://github.com/EvaSpessotto",
+  },
+  {
+    name: "Behance",
+    class: "behance",
+    icon: "fab fa-behance-square",
+    link: "https://www.behance.net/evaspessotto",
+  },
+  {
+    name: "Linkedin",
+    class: "linkedin",
+    icon: "fab fa-linkedin",
+    link: "https://www.linkedin.com/in/eva-spessotto/",
+  },
+]
+
+export default socialLinks
