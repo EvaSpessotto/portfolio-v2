9aa60b9f49f03012859bf8793152d538f5116a54
Spessotto <eva@spessotto.com>
2019-05-12 18:08:05 +0200

    Style: add custom colors for bg containers

--- src/templates/project-template.jsx
+++ src/templates/project-template.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import "./projectTemplate.scss"
 import { Container, Row, Col } from "reactstrap"
 import { graphql } from "gatsby"
 
@@ -17,35 +18,37 @@ export default function Template({ data }) {
         pathname={post.fields.slug}
         article
       />
-      <HeroPost banner={post.frontmatter.banner} />
+      <div className="project-container">
+        <HeroPost banner={post.frontmatter.banner} />
 
-      <Container>
-        <Row>
-          <Col>
-            <h1 className="mt-5">{post.frontmatter.title}</h1>
-            <p>{post.frontmatter.description}</p>
-          </Col>
-        </Row>
-      </Container>
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="mt-5">{post.frontmatter.title}</h1>
+              <p>{post.frontmatter.description}</p>
+            </Col>
+          </Row>
+        </Container>
 
-      {post.frontmatter.listImages.map(item => {
-        return (
-          <Container fluid style={{ background: `${item.bgColor}` }}>
-            <Row>
-              <Col>
-                <h2> {item.title}</h2>
-                <p>{item.description}</p>
-                <img
-                  src={item.image}
-                  alt={item.title}
-                  className="w-100"
-                  style={{ maxHeight: "400px" }}
-                />
-              </Col>
-            </Row>
-          </Container>
-        )
-      })}
+        {post.frontmatter.listImages.map(item => {
+          return (
+            <Container fluid className={`container-${item.bgColor} p-5`}>
+              <Row>
+                <Col>
+                  <h2> {item.title}</h2>
+                  <p>{item.description}</p>
+                  <img
+                    src={item.image}
+                    alt={item.title}
+                    className="w-100"
+                    style={{ maxHeight: "400px" }}
+                  />
+                </Col>
+              </Row>
+            </Container>
+          )
+        })}
+      </div>
     </Layout>
   )
 }
